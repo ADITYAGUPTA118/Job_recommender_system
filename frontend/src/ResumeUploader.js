e2ec2261095b7735a85a9499393c127f31586e53
ADITYAGUPTA118 <109325030+ADITYAGUPTA118@users.noreply.github.com>
2024-09-02 22:59:58 +0530

    Update ResumeUploader.js

--- frontend/src/ResumeUploader.js
+++ frontend/src/ResumeUploader.js
@@ -16,7 +16,7 @@ const ResumeUploader = () => {
     formData.append('resume', file);
 
     try {
-      const response = await axios.post('http://127.0.0.1:5000/recommend', formData, {
+      const response = await axios.post('process.env.REACT_APP_API_URL', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
